80a22993679272ca568a2003c797a315cda27035
Samir Ribeiro <samir.sjr77@gmail.com>
2025-06-08 13:40:05 -0300

    Format createdOn date

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,26 +1,29 @@
+import 'package:intl/intl.dart';
 import 'package:notes/helper/notesHelper.dart';
 
 class Note {
   int? id;
-  late String title, description, createOn;
+  late String title, description;
+  late DateTime _createOn;
 
   Note(this.title, this.description) {
-    createOn = DateTime.now().toString();
+    _createOn = DateTime.now();
   }
-
 
   Note.fromMap(Map map) {
     id = map[NotesHelper.columnId];
     title = map[NotesHelper.columnTitle];
     description = map[NotesHelper.columnDescription];
-    createOn = map[NotesHelper.columnCreatedOn];
+    _createOn = DateTime.parse(map[NotesHelper.columnCreatedOn]);
   }
+
+  String get createOn => DateFormat("dd/MM/y H:m").format(_createOn);
 
   Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
       NotesHelper.columnTitle: title,
       NotesHelper.columnDescription: description,
-      NotesHelper.columnCreatedOn: createOn
+      NotesHelper.columnCreatedOn: _createOn.toString()
     };
 
     if(id != null) {
